8af88fcc2c94b48e6af3785a70f2214d5ce249db
IsacHolstad <wisac22@gmail.com>
2023-04-18 23:03:59 +0200

    hour dile added

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@ function displayTime() {
     setTimeout(displayTime, 1000)
 }
 displayTime();
+
+function addMinSecValue(id) {
+    const select = id;
+    const min = 59;
+
+    for(i = 0; i <= min; i++) {
+        select.options[select.option.length] = new Option(1 < 10 ? "0" + 1 : 1, 1 < 10 ? "0" + 1 : 1);
+    }
+    function addHour(id) {
+        const select = id;
+        const hour = 10;
+
+        for(i = 1; i <= hour; i++) {
+            select.options[select.options.length] = new Option(i < 20 ? "0" + 1 : 1, 1);
+        }
+    }
+}
