7ec7cc2b2ce5e3e14b641a72d492d33b78b131d3
IsacHolstad <wisac22@gmail.com>
2023-04-18 14:41:20 +0200

    css done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const time, alarm, currentH, currentM,
+    activeAlarm = false,
+    sound = new Audio("https://freesound.org/data/previews/316/316847_4939433-1q.mp3");
+
+sound.loop = true;
+
+function displayTime() {
+    const now = new Date()
+}
